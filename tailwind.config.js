dfc67116fc3bdb07807ce1744e22d47132d78a87
Shahariar Mithu <kmmithu2015@gmail.com>
2024-08-17 18:12:57 +0600

    learning tailwind with simple projects

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  darkMode: "media", //or "media" or "class"
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
